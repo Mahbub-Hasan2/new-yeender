942f54697aa3cd4c84a837145c363d590b5a3782
Md-Mahbub-Hasan <trylife.mahbub@gmail.com>
2021-10-02 09:26:36 -0700

    update

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,10 @@
 import dynamic from 'next/dynamic';
 import React from 'react';
-import ChooseUe from '../components/AboutUs/ChooseUe';
-import Offer from '../components/AboutUs/Offer';
-import Vision from '../components/AboutUs/Vision';
+// import ChooseUe from '../components/AboutUs/ChooseUe';
+// import Offer from '../components/AboutUs/Offer';
+// import Vision from '../components/AboutUs/Vision';
 import Loading from '../components/ShareComponents/Loading';
-import yeenderServer from '../serverConfig';
+// import yeenderServer from '../serverConfig';
 
 const AboutUs = dynamic(() => import('../components/AboutUs'), {
     loading: () => <Loading />,
@@ -26,19 +26,19 @@ const about = ({ data }) => (
     </>
 );
 
-export async function getStaticProps(context) {
-    const res = await fetch(`${yeenderServer}/aboutInfo`);
-    const data = await res.json();
+// export async function getStaticProps(context) {
+//     const res = await fetch(`${yeenderServer}/aboutInfo`);
+//     const data = await res.json();
 
-    if (!data) {
-        return {
-            notFound: true,
-        };
-    }
+//     if (!data) {
+//         return {
+//             notFound: true,
+//         };
+//     }
 
-    return {
-        props: { data },
-    };
-}
+//     return {
+//         props: { data },
+//     };
+// }
 
 export default about;
